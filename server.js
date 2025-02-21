c6db68f6b0a6eaa2a669980341f44ac8d93b98ba
98hub <akun.sumberweb@gmail.com>
2025-02-21 10:09:40 +0700

    fix error result

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (_, res) => res.send('This app is running properly!'))
    .get('/dewaweb', (_, res) => res.send('Hello, Dewaweb Indonesia'))
-   .get('/dewacloud', (_, res) => res.send('Hello, Dewacloud indonesia'))
+   .get('/dewacloud', (_, res) => res.send('Hello, Dewacloud Indonesia'))
    .get('/hello/:name', (req, res) => res.json({ message: greeting(req.params.name) }));
 
 module.exports = app;
